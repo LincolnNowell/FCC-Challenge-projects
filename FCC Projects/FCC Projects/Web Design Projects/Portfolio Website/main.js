155ec67f8438be87563b3c91e185fe392b979200
Lincoln Nowell <lincolnnowell@gmail.com>
2019-12-08 02:26:45 -0600

    Add files via upload

--- FCC Projects/FCC Projects/Web Design Projects/Portfolio Website/main.js
+++ FCC Projects/FCC Projects/Web Design Projects/Portfolio Website/main.js
@@ -6,10 +6,16 @@ function hover(e){
     VideoPreview.id = e.target.src;
     VideoPreview.autoplay = true;
     VideoPreview.muted = true;
+    VideoPreview.controls = true;
+    VideoPreview.addEventListener('ended',(e)=>{
+        e.target.currentTime = 0;
+        e.target.play();
+    })
     document.getElementById(e.target.id).replaceWith(VideoPreview);
 }
 
 function restore(e){
+    console.log("Connected");
     let OriginalImage = document.createElement('img');
     OriginalImage.className = "card-img-top";
     OriginalImage.src = e.target.id;
@@ -22,7 +28,6 @@ function loadEvents(){
     //skip the title card
     for(let index = 1; index < card.length; index++){
         card.item(index).children[0].addEventListener("mouseenter",hover);
-        //card.item(index).children[0].addEventListener("mouseleave",restore);
     }
 }
 
